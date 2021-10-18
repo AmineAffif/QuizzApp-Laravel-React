18d3e5e4cc5fb1f48ebf1f25fd1b993660868738
Amine <affif.amine@live.fr>
2021-10-19 00:55:11 +0200

    added Auth0 Authentication (login / logout / register)

--- evoquiz-front/src/App.js
+++ evoquiz-front/src/App.js
@@ -1,29 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 
 import Homepage from './Components/Homepage';
 import Dashboard from './Components/Dashboard';
+import Header from './Components/Header';
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  BrowserRouter
 } from "react-router-dom";
+import Auth0ProviderWithHistory from './Auth0ProviderWithHistory';
 
 function App() {
   return (
-    <Router>
-      {/* Header will be here */}
-      <Switch>
-        <Route exact strict path="/">
-          <Homepage />
-        </Route>
-        <Route exact strict path="/dashboard">
-          <Dashboard />
-        </Route>
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Auth0ProviderWithHistory>
+      <Router>
+        {/* Header will be here */}
+        <Header />
+        <Switch>
+          <Route exact strict path="/">
+            <Homepage />
+          </Route>
+          <Route exact strict path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
+      </Auth0ProviderWithHistory>
+    </BrowserRouter>
   );
 }
 
--- evoquiz-front/src/Components/Header.js
+++ evoquiz-front/src/Components/Header.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import '../style/Header.css'
 import {
     Link
   } from "react-router-dom";
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Header = () => {
+    const {loginWithRedirect, logout, user, isLoading} = useAuth0();
     return (
-        <div>
-            
+        <div className="header_Wrapper">
+            {!isLoading && !user && (
+                <button 
+                    className="btn tbn-primary btn-block"
+                    onClick={() => loginWithRedirect()}
+                >
+                    Connexion
+                </button>
+                )
+            }
+            {!isLoading && user && (
+                <button 
+                    className="btn tbn-primary btn-block"
+                    onClick={() => logout()}
+                >
+                    Déconnexion
+                </button>
+                )
+            }
         </div>
     );
 };
