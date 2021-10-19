bcbc8767981f15f0ca47364d5d1657dc104b1d93
Amine <affif.amine@live.fr>
2021-10-19 11:41:39 +0200

    finished the Header frontend

--- evoquiz-front/src/Components/Header.js
+++ evoquiz-front/src/Components/Header.js
@@ -1,30 +1,53 @@
 import React from 'react';
 import '../style/Header.css'
 import {
-    Link
+    Link,
+    useHistory
   } from "react-router-dom";
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Header = () => {
     const {loginWithRedirect, logout, user, isLoading} = useAuth0();
+    const history = useHistory();
+
+    
     return (
         <div className="header_Wrapper">
+            {/* Main Logo */}
+            <Link to="/">
+                <img src="https://svgshare.com/i/bH8.svg" id="main_logo" alt="EvoQUIZ logo" />
+            </Link>
+
+            {/* Login Logout btn */}
             {!isLoading && !user && (
                 <button 
-                    className="btn tbn-primary btn-block"
+                    className="btn btn-light rounded-0 btn-block"
                     onClick={() => loginWithRedirect()}
                 >
                     Connexion
                 </button>
+                
                 )
             }
             {!isLoading && user && (
-                <button 
-                    className="btn tbn-primary btn-block"
+                <div className="nav_btn">
+                    <button 
+                    className="btn btn-dark rounded-0 btn-block"
+                    onClick={() => history.push("/dashboard")}
+                    >
+                        Mon profil
+                    </button>
+
+                    <button 
+                    className="btn btn-link text-dark rounded-0 btn-block text-decoration-none"
+                    id="logout_btn"
                     onClick={() => logout()}
-                >
-                    Déconnexion
-                </button>
+                    >
+                        Déconnexion
+                    </button>
+                
+                </div>
+                
                 )
             }
         </div>
