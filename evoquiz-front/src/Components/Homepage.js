ab7d6a0a79ea9b4dbea3303efdfc225533c69dd7
Amine <affif.amine@live.fr>
2021-10-19 16:22:50 +0200

    we can now loop and display foreach quiz - Fake temporary json data

--- evoquiz-front/src/Components/Homepage.js
+++ evoquiz-front/src/Components/Homepage.js
@@ -1,11 +1,204 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Homepage = () => {
-    return (
-        <div>
-            <h1>Homepage</h1>
-        </div>
-    );
+    
+  // All quiz
+  const [allQuiz, setAllQuiz] = useState([
+    {
+      quiz: [
+        {
+          title: "Quiz 1",
+          questions: [
+            {
+              content: "Quelle est l'heure actuelle en France ?",
+              answers: [
+                {
+                  content: "12h30",
+                  rightAnswer: true,
+                },
+                {
+                  content: "10h24",
+                  rightAnswer: false,
+                },
+                {
+                  content: "9h14",
+                  rightAnswer: false,
+                },
+              ],
+            },
+            {
+              content: "Quelle est l'heure actuelle en France ?",
+              answers: [
+                {
+                  content: "12h30",
+                  rightAnswer: true,
+                },
+                {
+                  content: "10h24",
+                  rightAnswer: false,
+                },
+                {
+                  content: "9h14",
+                  rightAnswer: false,
+                },
+              ],
+            },
+            {
+              content: "Quelle est l'heure actuelle en France ?",
+              answers: [
+                {
+                  content: "12h30",
+                  rightAnswer: true,
+                },
+                {
+                  content: "10h24",
+                  rightAnswer: false,
+                },
+                {
+                  content: "9h14",
+                  rightAnswer: false,
+                },
+              ],
+            },
+          ],
+        },
+
+        {
+            title: "Quiz 2",
+            questions: [
+              {
+                content: "Quelle est l'heure actuelle en France ?",
+                answers: [
+                  {
+                    content: "12h30",
+                    rightAnswer: true,
+                  },
+                  {
+                    content: "10h24",
+                    rightAnswer: false,
+                  },
+                  {
+                    content: "9h14",
+                    rightAnswer: false,
+                  },
+                ],
+              },
+              {
+                content: "Quelle est l'heure actuelle en France ?",
+                answers: [
+                  {
+                    content: "12h30",
+                    rightAnswer: true,
+                  },
+                  {
+                    content: "10h24",
+                    rightAnswer: false,
+                  },
+                  {
+                    content: "9h14",
+                    rightAnswer: false,
+                  },
+                ],
+              },
+              {
+                content: "Quelle est l'heure actuelle en France ?",
+                answers: [
+                  {
+                    content: "12h30",
+                    rightAnswer: true,
+                  },
+                  {
+                    content: "10h24",
+                    rightAnswer: false,
+                  },
+                  {
+                    content: "9h14",
+                    rightAnswer: false,
+                  },
+                ],
+              },
+            ],
+          },
+
+          {
+            title: "Quiz 3",
+            questions: [
+              {
+                content: "Quelle est l'heure actuelle en France ?",
+                answers: [
+                  {
+                    content: "12h30",
+                    rightAnswer: true,
+                  },
+                  {
+                    content: "10h24",
+                    rightAnswer: false,
+                  },
+                  {
+                    content: "9h14",
+                    rightAnswer: false,
+                  },
+                ],
+              },
+              {
+                content: "Quelle est l'heure actuelle en France ?",
+                answers: [
+                  {
+                    content: "12h30",
+                    rightAnswer: true,
+                  },
+                  {
+                    content: "10h24",
+                    rightAnswer: false,
+                  },
+                  {
+                    content: "9h14",
+                    rightAnswer: false,
+                  },
+                ],
+              },
+              {
+                content: "Quelle est l'heure actuelle en France ?",
+                answers: [
+                  {
+                    content: "12h30",
+                    rightAnswer: true,
+                  },
+                  {
+                    content: "10h24",
+                    rightAnswer: false,
+                  },
+                  {
+                    content: "9h14",
+                    rightAnswer: false,
+                  },
+                ],
+              },
+            ],
+          },
+
+
+      ],
+    },
+  ]);
+
+
+  return (
+    <div>
+      <h1>Homepage</h1>
+      {allQuiz.map((allQuiz) => {
+        return allQuiz.quiz.map((text) => {
+          console.log("FINAL");
+          console.log(text.title);
+          return (
+            <div>
+              <p>{text.title}</p>
+            </div>
+          );
+        });
+      })}
+    </div>
+  );
 };
 
 export default Homepage;
